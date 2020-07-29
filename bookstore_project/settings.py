ec527571b7b7c4a4f604874d9dba2aec32c49bf0
shubhamsks <shubhamkumar7051@gmail.com>
2020-07-29 22:38:57 +0530

    environment variables using django-environ

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -11,18 +11,22 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
+env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env('DEBUG')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8x=xd@io=lb&ymz!tv8dxlfj!ep%in*r+sf_djy*0i9fm&)=(4'
+SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
@@ -83,14 +87,7 @@ WSGI_APPLICATION = 'bookstore_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'books',
-        'USER': 'postgres',
-        'PASSWORD': 'sks',
-        'HOST': 'localhost',
-        'PORT':5432
-    }
+    'default': env.db()
 }
 
 
